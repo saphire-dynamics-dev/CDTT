a3af4c3ad8313f1f2c3d103b91212d94cd5a19a0
Saphire dynamics <info@saphiredynamicsolutions.com.ng>
2025-11-24 17:10:31 +0100

    small ui update v8

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,10 @@ export const metadata = {
   title: "Computerized drivers testing and training platform - CDTT",
   description: "The official Computerized drivers testing and training platform of Nigeria",
   icons: {
-    icon: "/cdtlogo2.png",
+    icon: {
+      url: "/cdtlogo2.png",
+      type: "image/png",
+    },
     shortcut: "/cdtlogo2.png",
     apple: "/cdtlogo2.png",
   },
@@ -37,8 +40,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/cdtlogo2.png" type="image/png" sizes="any" />
-        <link rel="apple-touch-icon" href="/cdtlogo2.png" />
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="any" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
